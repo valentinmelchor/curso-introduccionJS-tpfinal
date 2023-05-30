dd8513794f946fe5c43178e8148970765755e580
Valentín Melchor <valentinmelchor1401@gmail.com>
2023-05-29 22:19:58 -0300

    Cambio de API
    ES:
    Cambio de API en selector de paises.
    Selector de estados/provincias solo se muestra con Argentina y utiliza una API diferente.
    Cambios menores en CSS.
    
    EN:
    Changed API for country selector.
    Using different API for Argentina states, other countries disabled.
    Minor CSS changes.

--- TP Final/resources/form/js/form.js
+++ TP Final/resources/form/js/form.js
@@ -3,13 +3,21 @@ var x = 0; var y = 0; var xPos = 0; var yPos = 0;
 var speed = 0.05;
 var panel = $('.panel')[0];
 
+// Centrar panel de formulario
+function centerPanel() {
+    let temp = ($(window).height() / 2 - $(panel).height() / 2);
+    document.documentElement.style.setProperty("--panelOffset", temp.toFixed(2) + 'px');
+}
+
 // Capturar posicion del mouse relativa al panel y pasarla a porcentaje
 $(document).mousemove(function (e) {
     xPos = parseInt((((e.clientX - rect.left) / rect.width) * 100));
     yPos = parseInt((((e.clientY - rect.top) / rect.height) * 100));
 });
 
+// Funcion para actualizar propiedades en cada frame renderizado
 function Update() {
+    centerPanel();
     rect = panel.getBoundingClientRect();
     glassHiglightMotion();
 }
@@ -26,7 +34,6 @@ Update();
 // Destacar inputs en rojo al primer submit invalido y evitar el mensaje pop-up nativo del navegador
 $('input[required]').on('invalid', function (e) {
     $(this).addClass('failedSubmit');
-    // e.preventDefault();
 });
 
 // Animaciones formulario
@@ -41,50 +48,59 @@ $('input').blur(function () {
 });
 
 // Parametros API
-const COUNTRIES_BASE_URL = 'https://api.countrystatecity.in/v1/countries/';
-const API_KEY = 'd2VQdHBjQWRjWHRJRjhXRHFzMnczN3RYMmdrcE9KSWJMUmYwNVNscg==';
-function requestSettingsAPI(urlParameter) {
-    return { "url": urlParameter, "method": "GET", "headers": { "X-CSCAPI-KEY": API_KEY } };
+const COUNTRIES_BASE_URL = 'https://restcountries.com/v3.1/all/';
+const PROVINCES_BASE_URL = 'https://apis.datos.gob.ar/georef/api/provincias';
+const countryData = (index, array) => array[index].name.common;
+const provinceData = (index, array) => array[index].nombre;
+
+function requestSettingsAPI(urlParameter, data) {
+    return { "url": urlParameter, "method": "GET", "data": data, "dataType": "json" };
 }
-const statesURL = () => COUNTRIES_BASE_URL + $('#country').val() + '/states/';
-const citiesURL = () => statesURL() + $('#state').val() + '/cities/';
 
 // Actualizar lista de paises
 clearSelect('#country', 'Cargando lista de paises');
 console.time('Tiempo de retraso al cargar lista de paises');
-$.ajax(requestSettingsAPI(COUNTRIES_BASE_URL)).done(function (response) {
+$.ajax(requestSettingsAPI(COUNTRIES_BASE_URL, { fields: 'name' })).done(function (response) {
     clearSelect('#country', 'Seleccione un pais');
-    createOptions('#country', response);
+    createOptions('#country', response, sortCountries, countryData);
     console.timeEnd('Tiempo de retraso al cargar lista de paises');
 });
 
-// Actualizar lista de estados al recibir el pais seleccionado
+// Actualizar lista de provincias al recibir el pais seleccionado
 $('#country').change(function () {
-    clearSelect('#state', 'Cargando lista de estados/provincias');
-    console.time('Tiempo de retraso al cargar lista de estados');
-    $.ajax(requestSettingsAPI(statesURL())).done(function (response) {
-        clearSelect('#state', 'Seleccione un estado');
-        clearSelect('#city', 'Seleccione una ciudad');
-        createOptions('#state', response);
-        console.timeEnd('Tiempo de retraso al cargar lista de estados');
-    });
-});
-
-// Actualizar lista de ciudades al recibir el estado seleccionado
-$('#state').change(function () {
-    clearSelect('#city', 'Cargando lista de ciudades');
-    console.time('Tiempo de retraso al cargar lista de ciudades');
-    $.ajax(requestSettingsAPI(citiesURL())).done(function (response) {
-        clearSelect('#city', 'Seleccione una ciudad');
-        createOptions('#city', response);
-        console.timeEnd('Tiempo de retraso al cargar lista de ciudades');
-    });
+    clearSelect('#state', 'Cargando lista de provincias');
+    console.time('Tiempo de retraso al cargar lista de provincias');
+    if ($(this).val() == 'Argentina') {
+        $('#state').closest('fieldset').show();
+        centerPanel();
+        $.ajax(requestSettingsAPI(PROVINCES_BASE_URL, '')).done(function (response) {
+            clearSelect('#state', 'Seleccione una provincia');
+            createOptions('#state', response.provincias, sortStates, provinceData);
+            console.timeEnd('Tiempo de retraso al cargar lista de provincias');
+        });
+    } else {
+        $('#state').closest('fieldset').hide();
+        centerPanel();
+    }
 });
 
 // Funciones auxiliares
-function createOptions(selector, array) {
+function sortCountries(a, b) {
+    const nameA = a.name.common.toUpperCase();
+    const nameB = b.name.common.toUpperCase();
+    return (nameA > nameB) ? 1 : -1;
+}
+
+function sortStates(a, b) {
+    const nameA = a.nombre.toUpperCase();
+    const nameB = b.nombre.toUpperCase();
+    return (nameA > nameB) ? 1 : -1;
+}
+
+function createOptions(selector, array, sortFunction, filterFunction) {
+    let temp = array.sort(sortFunction);
     for (let i = 0; i < array.length; i++) {
-        $(selector).append($('<option>').text(array[i].name).val(array[i].iso2));
+        $(selector).append($('<option>').text(filterFunction(i, array)).val(filterFunction(i, array)));
     }
 }
 
--- TP Final/resources/form/js/form_backupAPI.js
+++ TP Final/resources/form/js/form_backupAPI.js
@@ -1,17 +1,50 @@
-// Aplicacion API publica "CountriesNow"
-// Utilizar en caso de que la API actual quede obsoleta, en mantenimiento o sin autorizacion
+// Aplicacion API "CountryStateCity"
 
-const COUNTRIES_BASE_URL = 'https://countriesnow.space/api/v0.1/countries/';
-
-function createOptionsByName(selector, array) {
-    for (let i = 0; i < array.length; i++) {
-        $(selector).append($('<option>').text(array[i].name).val(array[i].name));
-    }
+// Parametros API
+const COUNTRIES_BASE_URL = 'https://api.countrystatecity.in/v1/countries/';
+const API_KEY = 'd2VQdHBjQWRjWHRJRjhXRHFzMnczN3RYMmdrcE9KSWJMUmYwNVNscg==';
+function requestSettingsAPI(urlParameter) {
+    return { "url": urlParameter, "method": "GET", "headers": { "X-CSCAPI-KEY": API_KEY } };
 }
+const statesURL = () => COUNTRIES_BASE_URL + $('#country').val() + '/states/';
+const citiesURL = () => statesURL() + $('#state').val() + '/cities/';
 
+// Actualizar lista de paises
+clearSelect('#country', 'Cargando lista de paises');
+console.time('Tiempo de retraso al cargar lista de paises');
+$.ajax(requestSettingsAPI(COUNTRIES_BASE_URL)).done(function (response) {
+    clearSelect('#country', 'Seleccione un pais');
+    createOptions('#country', response);
+    console.timeEnd('Tiempo de retraso al cargar lista de paises');
+});
+
+// Actualizar lista de estados al recibir el pais seleccionado
+$('#country').change(function () {
+    clearSelect('#state', 'Cargando lista de estados/provincias');
+    console.time('Tiempo de retraso al cargar lista de estados');
+    $.ajax(requestSettingsAPI(statesURL())).done(function (response) {
+        clearSelect('#state', 'Seleccione un estado');
+        clearSelect('#city', 'Seleccione una ciudad');
+        createOptions('#state', response);
+        console.timeEnd('Tiempo de retraso al cargar lista de estados');
+    });
+});
+
+// Actualizar lista de ciudades al recibir el estado seleccionado
+$('#state').change(function () {
+    clearSelect('#city', 'Cargando lista de ciudades');
+    console.time('Tiempo de retraso al cargar lista de ciudades');
+    $.ajax(requestSettingsAPI(citiesURL())).done(function (response) {
+        clearSelect('#city', 'Seleccione una ciudad');
+        createOptions('#city', response);
+        console.timeEnd('Tiempo de retraso al cargar lista de ciudades');
+    });
+});
+
+// Funciones auxiliares
 function createOptions(selector, array) {
     for (let i = 0; i < array.length; i++) {
-        $(selector).append($('<option>').text(array[i]).val(array[i]));
+        $(selector).append($('<option>').text(array[i].name).val(array[i].iso2));
     }
 }
 
@@ -19,48 +52,3 @@ function clearSelect(selector, innerText) {
     $(selector).empty();
     $(selector).append($('<option value disabled selected>').text(innerText));
 }
-
-function sortObjectAlphabetically(array, property) {
-    return array.sort(function (a, b) {
-        var textA = a[property].toUpperCase();
-        var textB = b[property].toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    });
-}
-
-// Actualizar lista de paises
-$.get(COUNTRIES_BASE_URL + 'info', { returns: 'country' },
-    function (data) {
-        createOptionsByName('#country', sortObjectAlphabetically(data.data, 'name'));
-    },
-    "json"
-);
-
-// Actualizar estados al recibir pais
-$('#country').change(function () {
-    $.ajax({
-        type: "POST",
-        url: COUNTRIES_BASE_URL + 'states',
-        data: { country: $(this).val() },
-        dataType: "json",
-        success: function (response) {
-            clearSelect('#state', 'Seleccione un estado');
-            clearSelect('#city', 'Seleccione una ciudad');
-            createOptionsByName('#state', sortObjectAlphabetically(response.data['states'], 'name'));
-        }
-    });
-});
-
-// Actualizar ciudades al recibir estado
-$('#state').change(function (e) {
-    $.ajax({
-        type: "POST",
-        url: COUNTRIES_BASE_URL + 'state/cities',
-        data: { country: $('#country').val(), state: $(this).val() },
-        dataType: "json",
-        success: function (response) {
-            clearSelect('#city', 'Seleccione una ciudad');
-            createOptions('#city', response.data);
-        }
-    });
-});
