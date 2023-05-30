620888a3d942425b0c49a5f72d36cc192f696fae
Valentín Melchor <valentinmelchor1401@gmail.com>
2023-05-30 14:29:16 -0300

    Autocompletado de Localidad
    ES:
    Se cambió el elemento select usado para ciudad a una caja de texto para buscar con más rapidez la localidad y ahorrar recursos al pedir y cargar únicamente la búsqueda.
    Se cambió el elemento 'after' de los inputs requeridos a un asterisco rojo permanente para evitar confusiones y reducir la cantidad de elementos mostrados en pantalla.
    
    EN:
    City select element replaced with input type text with autocompletion to reduce visual clutter and CPU usage with a large list.
    After element for required input's changed to reduce visual clutter, before element removed.

--- TP Final/resources/form/js/form.js
+++ TP Final/resources/form/js/form.js
@@ -38,7 +38,7 @@ $('input[required]').on('invalid', function (e) {
 
 // Animaciones formulario
 $('input').focus(function () {
-    $(".animatedLabel[for='" + this.id + "']").css({ 'top': "5%", 'font-size': '0.8em', 'color': 'whitesmoke', 'font-style': 'italic' });
+    $(".animatedLabel[for='" + this.id + "']").css({ 'top': "5%", 'font-size': '0.8em', 'color': 'hsl(0deg, 0%, 75%)', 'font-style': 'italic' });
 });
 
 $('input').blur(function () {
@@ -49,9 +49,10 @@ $('input').blur(function () {
 
 // Parametros API
 const COUNTRIES_BASE_URL = 'https://restcountries.com/v3.1/all/';
-const PROVINCES_BASE_URL = 'https://apis.datos.gob.ar/georef/api/provincias';
-const countryData = (index, array) => array[index].name.common;
-const provinceData = (index, array) => array[index].nombre;
+const ARGENTINA_BASE_URL = 'https://apis.datos.gob.ar/georef/api/';
+const countryData = (index, array) => object = { text: array[index].name.common, value: array[index].name.common };
+const argentinaData = (index, array) => object = { text: array[index].nombre, value: array[index].id };
+var citySearchResult = [];
 
 function requestSettingsAPI(urlParameter, data) {
     return { "url": urlParameter, "method": "GET", "data": data, "dataType": "json" };
@@ -69,42 +70,107 @@ $.ajax(requestSettingsAPI(COUNTRIES_BASE_URL, { fields: 'name' })).done(function
 // Actualizar lista de provincias al recibir el pais seleccionado
 $('#country').change(function () {
     clearSelect('#state', 'Cargando lista de provincias');
+    clearList('#cityList'); citySearchResult = [];
+    $('#cityList').val('');
     console.time('Tiempo de retraso al cargar lista de provincias');
     if ($(this).val() == 'Argentina') {
         $('#state').closest('fieldset').show();
-        centerPanel();
-        $.ajax(requestSettingsAPI(PROVINCES_BASE_URL, '')).done(function (response) {
+        $.ajax(requestSettingsAPI(ARGENTINA_BASE_URL + 'provincias', '')).done(function (response) {
             clearSelect('#state', 'Seleccione una provincia');
-            createOptions('#state', response.provincias, sortStates, provinceData);
+            createOptions('#state', response.provincias, sortArgentina, argentinaData);
             console.timeEnd('Tiempo de retraso al cargar lista de provincias');
         });
     } else {
         $('#state').closest('fieldset').hide();
-        centerPanel();
+        $('#city').closest('fieldset').hide();
+    }
+});
+
+$('#state').change(function () {
+    if ($(this).val() > 0) {
+        $('#city').closest('fieldset').show();
+    } else {
+        $('#city').closest('fieldset').hide();
+    }
+    clearList('#cityList'); citySearchResult = [];
+    $('#city').val('');
+});
+
+var inputValue = '';
+$('#city').on('input', function () {
+    inputValue = $(this).val();
+    if (inputValue.length >= 4 && !$('#cityList').find('li').length > 0) {
+        console.time('Tiempo de retraso al cargar lista de ciudades');
+        $.ajax(requestSettingsAPI(ARGENTINA_BASE_URL + 'localidades', { provincia: $('#state').val(), nombre: inputValue, campos: 'nombre', max: 100 })).done(function (response) {
+            clearList('#cityList');
+            citySearchResult = response.localidades;
+            createClickableList('#cityList', response.localidades, sortArgentina, argentinaData);
+            console.timeEnd('Tiempo de retraso al cargar lista de ciudades');
+        });
+    } else if (inputValue.length > 4 && citySearchResult.length > 0) {
+        clearList('#cityList');
+        createClickableList('#cityList', citySearchResult.filter(searchInArray), sortArgentina, argentinaData);
+    } else if (!inputValue.length > 0) {
+        clearList('#cityList'); citySearchResult = [];
+    }
+});
+
+$('fieldset>ul').on('click', function () {
+    $('#city').val($('li:hover').text());
+    clearList('#cityList');
+    $('#email').focus();
+});
+
+$('#city').on('blur', function () {
+    if (!$('li:hover').length > 0) {
+        clearList('#cityList');
     }
 });
 
 // Funciones auxiliares
+function searchInArray(value) {
+    return value.nombre.includes(inputValue.toLocaleUpperCase());
+}
+
 function sortCountries(a, b) {
     const nameA = a.name.common.toUpperCase();
     const nameB = b.name.common.toUpperCase();
     return (nameA > nameB) ? 1 : -1;
 }
 
-function sortStates(a, b) {
+function sortArgentina(a, b) {
     const nameA = a.nombre.toUpperCase();
     const nameB = b.nombre.toUpperCase();
     return (nameA > nameB) ? 1 : -1;
 }
 
+function capitalizeString(string) {
+    let words = string.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toLocaleUpperCase() + words[i].slice(1, words[i].length).toLocaleLowerCase();
+    }
+    return words.join(' ');
+}
+
+function createClickableList(selector, array, sortFunction, filterFunction) {
+    let temp = array.sort(sortFunction);
+    for (let i = 0; i < Math.min(array.length, 5); i++) {
+        $(selector).append($('<li>').text(capitalizeString(filterFunction(i, array).text)));
+    }
+}
+
 function createOptions(selector, array, sortFunction, filterFunction) {
     let temp = array.sort(sortFunction);
     for (let i = 0; i < array.length; i++) {
-        $(selector).append($('<option>').text(filterFunction(i, array)).val(filterFunction(i, array)));
+        $(selector).append($('<option>').text(filterFunction(i, array).text).val(filterFunction(i, array).value));
     }
 }
 
 function clearSelect(selector, innerText) {
     $(selector).empty();
     $(selector).append($('<option value disabled selected>').text(innerText));
+}
+
+function clearList(selector) {
+    $(selector).empty();
 }
