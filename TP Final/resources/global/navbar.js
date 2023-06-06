fab07dd739a9f513a341e88f15fdc5bdc9b7197b
Valentín Melchor <valentinmelchor1401@gmail.com>
2023-06-05 21:05:35 -0300

    Firefox CSS Fix
    ES:
    - Se modifico uso innecesario de pseudoclases en el formulario que producian incompatibilidad con Firefox (Elementos de informacion ::after en inputs siguen siendo incompatibles)
    
    EN:
    - Removed some :has pseudo-classes in form page, improving compatibility with Firefox.

--- TP Final/resources/global/navbar.js
+++ TP Final/resources/global/navbar.js
@@ -1,12 +1,14 @@
-if (localStorage.getItem('name') != null) {
-    $('.login').hide();
-    $('.signup').hide();
-    $('.formB').append($('<label for="logout" style="margin: auto; margin-right:1ch">').text('Bienvenido, ' + localStorage.getItem('name') + '.'))
-        .append($('<button type="button" name="logout" id="logout">').text('Cerrar sesión'));
-    $('#logout').click(function () {
-        localStorage.clear();
-        window.location.href = './index.html';
-    });
-} else {
-    $('.sessionRequired').remove();
-}
+$(document).ready(function () {
+    if (localStorage.getItem('name') != null) {
+        $('.login').hide();
+        $('.signup').hide();
+        $('.formB').append($('<label for="logout" style="margin: auto; margin-right:1ch">').text('Bienvenido, ' + localStorage.getItem('name') + '.'))
+            .append($('<button type="button" name="logout" id="logout">').text('Cerrar sesión'));
+        $('#logout').click(function () {
+            localStorage.clear();
+            window.location.href = './index.html';
+        });
+    } else {
+        $('.sessionRequired').remove();
+    }
+});
